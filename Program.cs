507a24796dd99431e1eb17d9088095af981ac332
ArtemChekalev <76198345+ArtemChekalev@users.noreply.github.com>
2020-12-18 09:54:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Ханойский_башни
+{
+    class Program
+    {
+        static void move(int n, int i, int j, int k)
+        {
+            if (n == 1) 
+            {
+                Console.WriteLine($"{n} {i} {j}"); 
+            }
+            else
+            {
+                move(n - 1, i, k, j);
+                Console.WriteLine($"{n} {i} {j}");
+                move(n - 1, k, j, i);
+            }
+           
+        }
+        static void Main(string[] args)
+        {
+            move(4, 1, 3, 2);
+        }
+    }
+}
